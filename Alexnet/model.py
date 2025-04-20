e54c0e4bb21ae9aefd54672ab7de0ff9d4652916
Marko <marko.prohaska@gmail.com>
2025-04-20 21:00:14 +0200

    Reducing to 1-layer perceptron

--- Alexnet/model.py
+++ Alexnet/model.py
@@ -14,12 +14,12 @@ class MRNet3(nn.Module):
         
         # Add dropout for each view's features
         self.dropout_view1 = nn.Dropout(p=0.3)
-        self.dropout_view2 = nn.Dropout(p=0.3)
-        self.dropout_view3 = nn.Dropout(p=0.3)
+        # self.dropout_view2 = nn.Dropout(p=0.3)
+        # self.dropout_view3 = nn.Dropout(p=0.3)
 
         self.classifier1 = nn.Linear(int(256*3), 256)
-        self.dropout = nn.Dropout(p=0.5)
-        self.activation = nn.ReLU() 
+        # self.dropout = nn.Dropout(p=0.5)
+        # self.activation = nn.ReLU() 
         self.classifier2 = nn.Linear(256, 1)
 
     def forward(self, x):
@@ -56,9 +56,9 @@ class MRNet3(nn.Module):
             batch_features.append(x_stacked)
         
         x_stacked = torch.stack(batch_features)  # [batch_size, 768]
-        x_stacked = self.classifier1(x_stacked)
-        x_stacked = self.dropout(x_stacked)
-        x_stacked = self.activation(x_stacked)
+        # x_stacked = self.classifier1(x_stacked)
+        # x_stacked = self.dropout(x_stacked)
+        # x_stacked = self.activation(x_stacked)
         x_stacked = self.classifier2(x_stacked)  # [batch_size, 1]
         
         return x_stacked

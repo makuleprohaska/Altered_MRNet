4ee6faa4a7210e48c33c550217d2de915187e77f
Marko <marko.prohaska@gmail.com>
2025-04-24 13:29:02 +0200

    reducing stochasticity

--- Resnet/train.py
+++ Resnet/train.py
@@ -79,9 +79,11 @@ if __name__ == '__main__':
     
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
-    torch.backends.cudnn.deterministic = True
     if args.gpu and torch.cuda.is_available():
         torch.cuda.manual_seed_all(args.seed)
+        if torch.backends.cudnn.enabled:
+            torch.backends.cudnn.deterministic = True
+            torch.backends.cudnn.benchmark = False 
     elif args.mps and torch.backends.mps.is_available():
         pass
 

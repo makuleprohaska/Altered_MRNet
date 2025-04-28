1b5ba2617b7a6f7cbfcf04d518290531993cdaa9
Matteo <tatibruno2003@gmail.com>
2025-04-28 23:32:11 +0200

    removed dropout

--- Alexnet/model.py
+++ Alexnet/model.py
@@ -12,19 +12,19 @@ class MRNet3(nn.Module):
         self.model3 = models.alexnet(weights=AlexNet_Weights.DEFAULT)
         self.gap = nn.AdaptiveAvgPool2d(1)
 
-        # # Add batch normalization for each view's feature maps
-        # self.bn_view1 = nn.BatchNorm2d(256)  # AlexNet features output 256 channels
-        # self.bn_view2 = nn.BatchNorm2d(256)
-        # self.bn_view3 = nn.BatchNorm2d(256)
+        # Add batch normalization for each view's feature maps
+        self.bn_view1 = nn.BatchNorm2d(256)  # AlexNet features output 256 channels
+        self.bn_view2 = nn.BatchNorm2d(256)
+        self.bn_view3 = nn.BatchNorm2d(256)
         
-        # Add dropout for each view's features
-        self.dropout_view1 = nn.Dropout(p=0.2) 
-        self.dropout_view2 = nn.Dropout(p=0.2)
-        self.dropout_view3 = nn.Dropout(p=0.2)
+        # # Add dropout for each view's features
+        # self.dropout_view1 = nn.Dropout(p=0.2) 
+        # self.dropout_view2 = nn.Dropout(p=0.2)
+        # self.dropout_view3 = nn.Dropout(p=0.2)
 
         self.classifier1 = nn.Linear(int(256*3), 256)
         self.bn1 = nn.BatchNorm1d(256)  # BN after classifier1
-        self.dropout = nn.Dropout(p=0.2) # test
+        #self.dropout = nn.Dropout(p=0.2) # test
         self.activation = nn.ReLU() 
         self.classifier2 = nn.Linear(256, 1)
 
@@ -40,13 +40,13 @@ class MRNet3(nn.Module):
             
             if view == 0:
                 features = self.model1.features(x_view)
-                #features = self.bn_view1(features)
+                features = self.bn_view1(features)
             elif view == 1:
                 features = self.model2.features(x_view)
-                #features = self.bn_view2(features)
+                features = self.bn_view2(features)
             else:
                 features = self.model3.features(x_view)
-                #features = self.bn_view3(features)
+                features = self.bn_view3(features)
             
             features = self.gap(features).view(B, S_max, 256)  # [B, S_max, 256]
             s_indices = torch.arange(S_max, device=features.device).unsqueeze(0).expand(B, S_max)
@@ -54,12 +54,12 @@ class MRNet3(nn.Module):
             features = features.masked_fill(~mask.unsqueeze(2), -float('inf'))
             max_features = torch.max(features, dim=1)[0]  # [B, 256]
             
-            if view == 0:
-                max_features = self.dropout_view1(max_features)
-            elif view == 1:
-                max_features = self.dropout_view2(max_features)
-            else:
-                max_features = self.dropout_view3(max_features)
+            # if view == 0:
+            #     max_features = self.dropout_view1(max_features)
+            # elif view == 1:
+            #     max_features = self.dropout_view2(max_features)
+            # else:
+            #     max_features = self.dropout_view3(max_features)
             
             view_features.append(max_features)
         
@@ -69,7 +69,7 @@ class MRNet3(nn.Module):
         # Fully connected layers with BN
         x_stacked = self.classifier1(x_stacked)  # [B, 256]
         x_stacked = self.bn1(x_stacked)  # Apply batch normalization
-        x_stacked = self.dropout(x_stacked)
+        #x_stacked = self.dropout(x_stacked)
         x_stacked = self.activation(x_stacked)
         x_stacked = self.classifier2(x_stacked)  # [B, 1]
         

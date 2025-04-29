a969a914ce9b4518d9d6de6689ea35001378c008
Matteo <tatibruno2003@gmail.com>
2025-04-29 23:04:31 +0200

    bug fixed

--- Alexnet_Majority/evaluate.py
+++ Alexnet_Majority/evaluate.py
@@ -8,6 +8,7 @@ from torch.autograd import Variable
 from tqdm import tqdm
 from loader import load_data3
 from model import MRNet3
+from torch.cuda.amp import autocast
  
 
 
@@ -63,7 +64,7 @@ def run_model(model, loader, train=False, optimizer=None, eps: float = 0.0):
         label = label.to(device)                       # [batch_size,1]
         vol_lists = [[view.to(device) for view in views] for views in vol_lists]
 
-        with torch.autocast(enabled=True):
+        with torch.autocast():
             logits = model(vol_lists)                      # [batch_size,1]
         probs  = torch.sigmoid(logits)                 # [batch_size,1]
 

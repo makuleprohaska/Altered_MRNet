8a58aeac680511f371d1fcece9148e8faa111ccd
Matteo <tatibruno2003@gmail.com>
2025-04-29 19:40:46 +0200

    updates

--- Alexnet_Majority/train.py
+++ Alexnet_Majority/train.py
@@ -18,13 +18,13 @@ def get_device(use_gpu, use_mps):
     else:
         return torch.device("cpu")
 
-def train3(rundir, epochs, learning_rate, use_gpu, use_mps, data_dir, labels_csv, weight_decay, max_patience, epsilon):
+def train3(rundir, epochs, learning_rate, use_gpu, use_mps, data_dir, labels_csv, weight_decay, max_patience, batch_size, augment, epsilon):
     device = get_device(use_gpu, use_mps)
     print(f"Using device: {device}")
-    train_loader, valid_loader = load_data3(device, data_dir, labels_csv, batch_size=args.batch_size, augment=args.augment)
+    train_loader, valid_loader = load_data3(device, data_dir, labels_csv, batch_size=batch_size, augment=augment)
     
     #This now deals with the case that batch size is 1
-    use_batchnorm = args.batch_size > 1
+    use_batchnorm = batch_size > 1
     model = MRNet3(use_batchnorm=use_batchnorm)
     model = model.to(device)
 
@@ -33,7 +33,7 @@ def train3(rundir, epochs, learning_rate, use_gpu, use_mps, data_dir, labels_csv
     optimizer = torch.optim.Adam(model.parameters(), learning_rate, weight_decay=weight_decay)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=max_patience, factor=.3, threshold=1e-4)
 
-    best_val_loss = float('inf')
+    best_val_auc = float('-inf')
 
     start_time = datetime.now()
 
@@ -53,11 +53,11 @@ def train3(rundir, epochs, learning_rate, use_gpu, use_mps, data_dir, labels_csv
 
         scheduler.step(val_loss)
 
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            file_name = f'val{val_loss:0.4f}_train{train_loss:0.4f}_epoch{epoch+1}'
+        if val_auc > best_val_auc:
+            best_val_auc = val_auc
+            file_name = f'val{val_auc:0.4f}_train{train_auc:0.4f}_epoch{epoch+1}'
             save_path = Path(rundir) / file_name 
-            # -torch.save(model.state_dict(), save_path)
+            torch.save(model.state_dict(), save_path)
 
         # Log metrics to file
         with open(os.path.join(rundir, 'metrics.txt'), 'a') as f:
@@ -72,13 +72,13 @@ def get_parser():
     parser.add_argument('--gpu', action='store_true', help='Use CUDA if available')
     parser.add_argument('--mps', action='store_true', help='Use MPS if available')
     parser.add_argument('--learning_rate', default=1e-05, type=float)
-    parser.add_argument('--weight_decay', default=0.01, type=float)
+    parser.add_argument('--weight_decay', default=0.0025, type=float)
     parser.add_argument('--epochs', default=50, type=int)
     parser.add_argument('--max_patience', default=5, type=int)
     parser.add_argument('--factor', default=0.3, type=float)
     parser.add_argument('--batch_size', default=1, type=int, help='Batch size for training and validation')
     parser.add_argument('--eps', default=0.0, type=float, help='Label smoothing factor (0.0 = no smoothing)')
-    parser.add_argument('--augment', action='store_true', help='Apply data augmentation during training')
+    parser.add_argument('--augment', default=True, action='store_true', help='Apply data augmentation during training')
     return parser
 
 if __name__ == '__main__':
@@ -97,7 +97,7 @@ if __name__ == '__main__':
         json.dump(vars(args), out, indent=4)
 
         train3(args.rundir, args.epochs, args.learning_rate, 
-           args.gpu, args.mps, args.data_dir, args.labels_csv, args.weight_decay, args.max_patience, args.eps)
+           args.gpu, args.mps, args.data_dir, args.labels_csv, args.weight_decay, args.max_patience, args.batch_size, args.augment, args.eps)
     
 #to run use
 """
